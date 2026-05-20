28b784139e32da31d9f6a68c906e8ea1dc3901e1
Educatori <educatori127@gmail.com>
2026-05-21 01:33:41 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,7 +6,19 @@
 import { initializeApp, getApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getDatabase, ref, get, update, set, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import { firebaseConfig } from "./firebase-config.js";
+
+// ── CONFIGURAZIONE FIREBASE (inserisci qui i tuoi dati) ──
+const firebaseConfig = {
+    apiKey: "AIzaSyA2CZmgpuWsaLXiKoSQmnigf67MJI44Rus",
+  authDomain: "anagrafica25.firebaseapp.com",
+  databaseURL: "https://anagrafica25-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "anagrafica25",
+  storageBucket: "anagrafica25.firebasestorage.app",
+  messagingSenderId: "724474113103",
+  appId: "1:724474113103:web:065ae533efb97d7a080e3a",
+  measurementId: "G-MBZW81SSH6"
+};
+ 
 
 // ── Inizializza Firebase ────────────
 let app;
