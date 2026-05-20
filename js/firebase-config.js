91c296388e50f75aa1e9406b3d20d70a4e54abe8
Educatori <educatori127@gmail.com>
2026-05-20 12:03:00 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,14 +2,14 @@
 // Configurazione Firebase unificata per tutto il sito
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyA2CZmgpuWsaLXiKoSQmnigf67MJI44Rus",
-    authDomain: "anagrafica25.firebaseapp.com",
-    databaseURL: "https://anagrafica25-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "anagrafica25",
-    storageBucket: "anagrafica25.firebasestorage.app",
-    messagingSenderId: "724474113103",
-    appId: "1:724474113103:web:065ae533efb97d7a080e3a",
-    measurementId: "G-MBZW81SSH6"
+  apiKey: "AIzaSyA2CZmgpuWsaLXiKoSQmnigf67MJI44Rus",
+  authDomain: "anagrafica25.firebaseapp.com",
+  databaseURL: "https://anagrafica25-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "anagrafica25",
+  storageBucket: "anagrafica25.firebasestorage.app",
+  messagingSenderId: "724474113103",
+  appId: "1:724474113103:web:065ae533efb97d7a080e3a",
+  measurementId: "G-MBZW81SSH6"
 };
 
 // Opzionale: esporta anche i path dei database per coerenza
